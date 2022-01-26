b6a552541f99eb7908f57dca682ef8016a47397b
ragnarlodbrok1992 <maraus92@gmail.com>
2022-01-26 22:41:57 +0100

    Initial commit of console function

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,7 +1,5 @@
 #include "Console.h"
 
-// TEMP include
-#include <iostream>
 
 Console::Console(int x, int y, int width, int height) {
 
@@ -10,9 +8,6 @@ Console::Console(int x, int y, int width, int height) {
 Console::~Console() {};
 
 void Console::render(bool should_render) {
-
-  // Temp debug code
-  // std::cout << "Render time: " << this->render_time << std::endl;
   WriteMessage("Render time: ", this->render_time, 20, 300);
 
 };
