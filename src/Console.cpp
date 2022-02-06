959a126395749efb9f741be357c49de29e8bbfa0
ragnarlodbrok1992 <maraus92@gmail.com>
2022-02-07 00:19:01 +0100

    Rendering console and internat console counter

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,13 +1,25 @@
 #include "Console.h"
 
 
-Console::Console(int x, int y, int width, int height) {
-
+Console::Console(float x, float y, float width, float height) {
+  this->rect = {x, y, width, height};
 };
 
 Console::~Console() {};
 
 void Console::render(bool should_render) {
   WriteMessage("Render time: ", this->render_time, 20, 300);
+  WriteMessage("Should render: ", should_render, 20, 320);
+
+  // Actual render code goes here
+  if (should_render) {
+    // Increment counter
+    this->render_time++;
+
+    // Render console proper
+    DrawRectangleRec(this->rect, this->blue_soft_transparent);
+  } else {
+    this->render_time = 0;
+  }
 
 };
--- src/Console.h
+++ src/Console.h
@@ -5,12 +5,16 @@
 
 class Console {
   public:
-    int x, y, width, height;
+    // Console colors and stuff
+    Color blue_soft_transparent = {0, 121, 241, 127};
+
+    // Console geometry
+    Rectangle rect = {};
     unsigned int render_time = 0;
-    Console(int x,
-            int y,
-            int width,
-            int height);
+    Console(float x,
+            float y,
+            float width,
+            float height);
     ~Console();
 
     void render(bool should_render);
